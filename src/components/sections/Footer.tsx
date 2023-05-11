2aa78aa5a237cdd8f1b00ce912766f5fe735256b
Onakoya Kayode <macbook@macbooks-MacBook-Pro.local>
2023-05-11 22:39:11 +0100

    update footer

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='w-full flex flex-col items-center py-[20px] md:py-[30px] lg:py-[40px] xl:py-[50px]'>
       <div className='w-full border border-zinc-500 mb-10'></div>
       <div className='flex items-end justify-between w-full px-3 md:px-[20px] lg:px-[50px]'>
-        <div className='flex flex-col items-start gap-6 md:gap-10'>
+        <div className='flex flex-col w-[50%] items-start gap-6 md:gap-10'>
             <h1 className="text-[20px] md:text-[24px] lg:text-[30px]">LET&apos;S CONNECT</h1>
             <ul className=' flex flex-col items-start md:flex md:items-center gap-3 md:gap-5 text-[#8492a6]'>
                 <Link target='_blank' className='cursor-pointer text-[12px] md:text-[14px] lg:text-[15px] hover:text-[#25D366] ease-in-out duration-150' href='https://wa.link/0k4ks7'>Whatsapp</Link>
@@ -14,8 +14,8 @@ const Footer = () => {
                 <Link target='_blank' className='cursor-pointer text-[12px] md:text-[14px] lg:text-[15px] hover:text-[#cc2366] ease-in-out duration-150' href='https://www.instagram.com/onakoya_kayode'>Instagram</Link>
             </ul>
         </div>
-        <div>
-            <p className='text-[11px] md:text-[12px] lg:text-[13px]'>Developed by <Link href='https://github.com/onakoyakayode' className='text-[#f26e49] text-[14px] md:text-[18px] lg:text[22px] hover:underline'>Onakoya Kayode</Link></p>
+        <div className='flex items-center justify-center w-[50%]'>
+            <p className='text-[11px] md:text-[12px] lg:text-[13px] flex flex-col'>Developed by <Link href='https://github.com/onakoyakayode' className='text-[#f26e49] text-[14px] md:text-[18px] lg:text[22px] hover:underline'>Onakoya Kayode</Link></p>
         </div>
       </div>
     </div>

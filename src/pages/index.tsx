10ea8494d8d93d021e9a96e2028d75ccadefb118
Onakoya Kayode <macbook@macbooks-MacBook-Pro.local>
2023-05-15 10:01:35 +0100

    changed page name to portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   useEffect(() => {
     if (!welcomeScreen) {
-      router.push('/mainpage');
+      router.push('/portfolio');
     }
   }, [router, welcomeScreen])
 
